5b8d97b80609404fabaf1c22fbac863a782f7609
Ajai Kumar <ajikuamr@gmail.com>
2023-06-18 08:54:30 +0530

    fix some erors

--- backend/middleware/GetjwtResponse.js
+++ backend/middleware/GetjwtResponse.js
@@ -4,7 +4,7 @@ const GetJwtResponse = (user,statuscode,res)=>{
     const options = {
         expires:new Date(Date.now() + process.env.COOOKIE_EXPIRES * 24 * 60 * 60 * 1000),
         httpOnly:true,
-        sameSite:"none",
+        sameSite:"lax",
         path:"/",
         secure:false
     }
